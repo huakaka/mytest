6ab0234e46f2e6a335b801e12375494cfde0a3bf
huakaka <2549617343@qq.com>
2019-02-28 19:54:43 +0800

    修改test文件:添加输出行

--- idea-git-test/src/test.java
+++ idea-git-test/src/test.java
@@ -3,5 +3,10 @@ public class test {
         System.out.println("hello git");
         System.out.println("添加test");
         System.out.println("hello git2");
+
+
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
     }
 }
